ef549937bf3961be9fa33860a76dc87af06b57f4
Chris Thomas <chris.thomas@antimatter-studios.com>
2026-09-23 14:37:54 +0100

    test: select entry editor in vault view tests

--- trove-desktop/tests/VaultView.test.jsx
+++ trove-desktop/tests/VaultView.test.jsx
@@ -257,7 +257,7 @@ describe('unlocked vault interactions', () => {
 
     fireEvent.click(c.querySelectorAll('.list .erow')[0]);
     await waitFor(() => expect(api.getEntryDetail).toHaveBeenCalled());
-    const edit = [...c.querySelectorAll('button')].find((b) => /edit/i.test(b.textContent || b.title || ''));
+    const edit = [...c.querySelectorAll('.detail button')].find((b) => /edit/i.test(b.textContent || b.title || ''));
     fireEvent.click(edit);
     await waitFor(() => expect(c.querySelector('.modal')).toBeTruthy());
     const save = [...c.querySelectorAll('.modal button')].find((b) => /save changes/i.test(b.textContent));
@@ -273,7 +273,7 @@ describe('unlocked vault interactions', () => {
     const c = await mountUnlocked();
     fireEvent.click(c.querySelectorAll('.list .erow')[0]);
     await waitFor(() => expect(api.getEntryDetail).toHaveBeenCalled());
-    const edit = [...c.querySelectorAll('button')].find((b) => /edit/i.test(b.textContent || b.title || ''));
+    const edit = [...c.querySelectorAll('.detail button')].find((b) => /edit/i.test(b.textContent || b.title || ''));
     fireEvent.click(edit);
     await waitFor(() => expect(c.querySelector('.modal')).toBeTruthy());
 
@@ -294,7 +294,7 @@ describe('unlocked vault interactions', () => {
     const c = await mountUnlocked();
     fireEvent.click(c.querySelectorAll('.list .erow')[0]);
     await waitFor(() => expect(api.getEntryDetail).toHaveBeenCalled());
-    const edit = [...c.querySelectorAll('button')].find((b) => /edit/i.test(b.textContent || b.title || ''));
+    const edit = [...c.querySelectorAll('.detail button')].find((b) => /edit/i.test(b.textContent || b.title || ''));
     fireEvent.click(edit);
     await waitFor(() => expect(c.querySelector('.modal')).toBeTruthy());
     const pathOf = (el) => el.querySelector('input.mono').value;
